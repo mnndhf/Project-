5c57ca8bc72d2d04fd37e89afb3cb9b41897267a
Muna Nadhifa <nadhifa.aux@loket.com>
2019-02-22 21:05:11 +0700

    benerinjs

--- project.js
+++ project.js
@@ -1,42 +1,57 @@
 //buat add todolist
 
-  function newElement(){
-    var input = document.getElementById('tdl').value;
-    var p = document.createElement('p');
-    var text=document.createTextNode(input);
-    p.appendChild(text);
-    document.getElementById('list').appendChild(p);
-    
-//buat bikin button remove dan function remove
-    var removeTask = document.createElement('input');
-    removeTask.setAttribute('type', 'button');
-    removeTask.setAttribute("value", "Delete");
-    removeTask.setAttribute("id", "removeButton");
-    removeTask.addEventListener('click', function(e) {
-        p.parentNode.removeChild(p);
-    }, false);
-    p.appendChild(removeTask);    
-    
-//buat bikin button edit 
-    var editTask = document.createElement('input');
-    editTask.setAttribute('type', 'button');
-    editTask.setAttribute("value", "Edit");
-    editTask.setAttribute("id", "EditButton");
-    editTask.addEventListener('click', function(e) {
+function newElement() {
+  var input = document.getElementById("tdl").value;
+  var p = document.createElement("p");
+  var par = document.createElement("h6");
+  var text = document.createTextNode(input);
+  par.appendChild(text);
+  p.appendChild(par);
+  document.getElementById("list").appendChild(p);
+
+  //buat bikin button remove dan function remove
+  var removeTask = document.createElement("input");
+  removeTask.setAttribute("type", "button");
+  removeTask.setAttribute("value", "Delete");
+  removeTask.setAttribute("id", "removeButton");
+  removeTask.addEventListener(
+    "click",
+    function(e) {
+      p.parentNode.removeChild(p);
+    },
+    false
+  );
+  p.appendChild(removeTask);
+
+  //buat bikin button edit
+  var editTask = document.createElement("input");
+  editTask.setAttribute("type", "button");
+  editTask.setAttribute("value", "Edit");
+  editTask.setAttribute("id", "EditButton");
+  editTask.addEventListener(
+    "click",
+    function(e) {
+      // console.log(p.children[0])
       var x = document.createElement("INPUT");
       x.setAttribute("type", "text");
-      x.setAttribute("value" , "");
+      x.setAttribute("value", p.children[0].innerHTML);
       p.appendChild(x);
-      p.parentNode.editTask(p);
-    
-    }, false);
-    p.appendChild(editTask);
-    
- 
-    
- 
+      var saveTask = document.createElement("input");
+      saveTask.setAttribute("type", "button");
+      saveTask.setAttribute("value", "Save");
+      saveTask.setAttribute("id", "SaveButton");
+      saveTask.addEventListener(
+        "click",
+        function(e) {
+           
+        
+        },
+      );
+      p.appendChild(saveTask);
+    },
+    false
+  );
+  p.appendChild(editTask);
 }
-
-
 
 
